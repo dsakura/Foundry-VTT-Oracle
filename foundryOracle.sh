c66a6afb8169f477899d4082f6d88c6507a78679
Daniel Nach <dan.nachmanowicz@gmail.com>
2023-07-21 23:24:20 -0300

    Update foundryOracle.sh

--- foundryOracle.sh
+++ foundryOracle.sh
@@ -1,55 +1,55 @@
 #!/bin/bash    
 # chmod a+x /home/ubuntu/foundryOracle.sh
-# Foundry Installation on Oracle
-# update the system and remove older packages
+# Instalação do Foundry na Oracle e AWS
+# atualiza o sistema e remove os pacotes mais antigos
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
-#update the iptables to open ports 80, 443, 30000
+#update do iptables para abrir portas 80, 443, 30000
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --match multiport --dports 80,443,30000 -j ACCEPT
-# save this configuration
+# salva esta config
 sudo netfilter-persistent save
-# install nodejs, pm2, nano, unzip
+# instala nodejs, pm2, nano, unzip
 sudo apt install curl nano unzip -y
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install pm2 -g
 sudo npm update -g pm2
 sudo pm2 update
-#allow pm2 to start and stop after reboot
+#permite que o pm2 inicie e pare após a reinicialização
 pm2 startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-# make the needed directory
+# cria diretorios necessarios
 mkdir ~/foundry
 mkdir ~/foundryuserdata
-# Install caddy for reverse proxy and https configuration
+# Instala o caddy para reverse proxy e configuracoes https
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
-# Enter Foundry timed url for download
-echo "please enter the foundry vtt timed download url for the NodeJS version"
+# Insere o Timed URL do Foundry para download
+echo "insira o Timed URL de download do foundry vtt na versão NodeJS"
 read tdurl
 wget --output-document ~/foundry/foundryvtt.zip "$tdurl"
 unzip ~/foundry/foundryvtt.zip -d ~/foundry/
 rm ~/foundry/foundryvtt.zip
-# Set up pm2 to start foundry vtt at system startup or reboot.
+# Configura o pm2 para iniciar o Foundry vtt na inicialização ou reinicialização do sistema.
 pm2 start "node /home/ubuntu/foundry/resources/app/main.js --dataPath=/home/ubuntu/foundryuserdata" --name foundry
 pm2 save
-# Setting up Caddy reverse proxy
+# Configurando o proxy reverso do Caddy
 curl -o Caddyfile https://raw.githubusercontent.com/aco-rt/Foundry-VTT-Oracle/main/Caddyfile
 sudo rm /etc/caddy/Caddyfile
 sudo mv Caddyfile /etc/caddy/Caddyfile
-echo "please enter the domain url your players will use to connect to your server"
+echo "Insira o url do domínio do servidor"
 read vtturl
 sudo sed -i "s/your.hostname.com/$vtturl/g" /etc/caddy/Caddyfile
 sudo service caddy restart
-# Edit foundry options.json file to allow connections through proxy and 443
+# Edita o arquivo foundry options.json para permitir conexões por meio de proxy e 443
 sed -i 's/"proxyPort": null/"proxyPort": 443/g' /home/ubuntu/foundryuserdata/Config/options.json
 sed -i 's/"proxySSL": false/"proxySSL": true/g' /home/ubuntu/foundryuserdata/Config/options.json
 sed -i 's/"hostname": null/"hostname": "$vtturl"/g' /home/ubuntu/foundryuserdata/Config/options.json
-# Restarting the system to complete installation
+# Reinicia o sistema para concluir a instalação
 sleep 2
 clear
-echo "Restarting the system to complete installation"
+echo "Reinicia o sistema para concluir a instalação"
 sleep 3
 sudo shutdown -r now
