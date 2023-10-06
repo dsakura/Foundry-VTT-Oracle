3d3cb0500b4832478864a003857c93cd9ccd2fc7
Daniel Nach <dan.nachmanowicz@gmail.com>
2023-10-06 17:44:58 -0300

    Update atualizarNode.sh

--- atualizarNode.sh
+++ atualizarNode.sh
@@ -2,9 +2,13 @@
 # chmod a+x /home/ubuntu/atualizarNode.sh
 pm2 stop all
 pm2 unstartup
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt upgrade
+sudo apt install -y nodejs
 npm rebuild -g pm2
 pm2 startup
 pm2 start all
